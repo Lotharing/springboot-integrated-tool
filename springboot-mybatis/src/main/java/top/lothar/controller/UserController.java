f4c0aa25a3dcc625fe3a13e9b9aba5b524ffe1ea
Lotharing <809676295@qq.com>
2020-03-14 22:47:44 +0800

    MybatisPageHelper

--- springboot-mybatis/src/main/java/top/lothar/controller/UserController.java
+++ springboot-mybatis/src/main/java/top/lothar/controller/UserController.java
@@ -1,6 +1,9 @@
 package top.lothar.controller;
 
 
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,6 +23,37 @@ public class UserController {
     private String getUserList(){
         List<User> user = userMapper.getUserList();
         return user.toString();
+    }
+
+    /**
+     * PageHelper 分页插件
+     * @param pageNum
+     * @param pageSize
+     * @return
+     */
+    @RequestMapping("/getPageUserList")
+    private Page<User> getPageUserList(Integer pageNum,Integer pageSize){
+        PageHelper.startPage(pageNum,pageSize);
+        Page<User> user = userMapper.getPageUserList();
+        System.out.println("总数量：" + user.getTotal());
+        System.out.println("当前页查询记录：" + user.getResult().size());
+        System.out.println("当前页码：" + user.getPageNum());
+        System.out.println("每页显示数量：" + user.getPageSize());
+        System.out.println("总页：" + user.getPages());
+        return user;
+    }
+
+    @RequestMapping("/getPageUserListExample")
+    private PageInfo<User> getPageUserListExample(Integer pageNum,Integer pageSize){
+        PageHelper.startPage(pageNum,pageSize);
+        List<User> userList = userMapper.getPageUserListExample();
+        PageInfo<User> page = new PageInfo<>(userList);
+        System.out.println("总数量：" + page.getTotal());
+        System.out.println("当前页查询记录：" + page.getList().size());
+        System.out.println("当前页码：" + page.getPageNum());
+        System.out.println("每页显示数量：" + page.getPageSize());
+        System.out.println("总页：" + page.getPages());
+        return page;
     }
 
 }
--- springboot-mybatis/src/main/java/top/lothar/dao/UserMapper.java
+++ springboot-mybatis/src/main/java/top/lothar/dao/UserMapper.java
@@ -1,6 +1,8 @@
 package top.lothar.dao;
 
 
+import com.github.pagehelper.Page;
+import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Repository;
 import top.lothar.entity.User;
 import java.util.List;
@@ -9,5 +11,11 @@ import java.util.List;
 public interface UserMapper {
 
     List<User> getUserList();
+
+    @Select("SELECT * FROM sd_sys_user")
+    Page<User> getPageUserList();
+
+    @Select("SELECT * FROM sd_sys_user")
+    List<User> getPageUserListExample();
 
 }
