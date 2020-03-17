d3fdc376274ad85755336ffdf0edac3d7a9f0cc5
Lotharing <809676295@qq.com>
2020-03-17 22:31:58 +0800

    springboot2.2.4+scheduled

--- springboot-scheduled/src/main/java/top/lothar/timer/Timer.java
+++ springboot-scheduled/src/main/java/top/lothar/timer/Timer.java
@@ -1,5 +1,20 @@
 package top.lothar.timer;
 
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
 public class Timer {
+
+    /**
+     * cron 表达式 每秒执行一次
+     */
+    @Scheduled(cron = "0/1 * * * * ?")
+    private void test() {
+        System.out.println("执行定时任务的时间是："+new Date());
+    }
 
 }
