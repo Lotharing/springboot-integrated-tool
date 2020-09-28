d8fd6367a8aaced6369edd78b7fadd75ae6b68f8
lutong <809676295@qq.com>
2020-09-28 10:59:03 +0800

    搜索请求对象标注/SearchSourceBuilder搜索内容参数配置

--- springboot-elasticsearch/src/main/java/top/lothar/entity/Teacher.java
+++ springboot-elasticsearch/src/main/java/top/lothar/entity/Teacher.java
@@ -71,14 +71,24 @@ public class Teacher implements Serializable {
         page = page > 0 ? (page - 1) * size : page * size;
         size = size > 0 ? size : 10;
         //老师 1  索引
+        /**
+         * 创建搜索请求对象
+         * new SearchRequest("posts","posts"); 查询多个文档库
+         *
+         */
         SearchRequest request = new SearchRequest(EsTypeEnum.TEACHER.index());
-
         Map<String, Float> fields = new HashMap<>();
         fields.put("name", new Float(6.0));
         fields.put("remark", new Float(1.0));
-
+        /**
+         * 创建  搜索内容参数设置对象:SearchSourceBuilder
+         * 相对于matchQuery，multiMatchQuery针对的是多个fi eld，也就是说，当multiMatchQuery中，fieldNames参数只有一个时，其作用与matchQuery相当；
+         * 而当fieldNames有多个参数时，如field1和field2，那查询的结果中，要么field1中包含text，要么field2中包含text。
+         */
         QueryBuilder queryBuilders=  QueryBuilders.multiMatchQuery(keyword, "name", "remark").fields(fields);
+        //设置查询对象
         SearchSourceBuilder searchSourceBuilder = ElasticUtil.initSearchSourceBuilder(queryBuilders, page, size);
+        //查询对象封进搜索请求对象
         request.source(searchSourceBuilder);
         return request;
     }
